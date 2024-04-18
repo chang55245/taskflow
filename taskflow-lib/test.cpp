488674285f89945a1867bdc593e68c035b9250f6
chang55245 <lchang21@asu.edu>
2024-04-18 14:53:22 -0700

    add test

--- taskflow-lib/test.cpp
+++ taskflow-lib/test.cpp
@@ -8,9 +8,11 @@ void test()
 }
 int main(){
     auto myTaskflowLib = taskflowLib();
+    // auto A = myTaskflowLib.task_definition("A", test);
+    // auto A = myTaskflowLib.task_definition(NULL, nullptr);
+    // auto A = myTaskflowLib.task_definition("A", [](){printf("A\n");});
+    // auto A = myTaskflowLib.task_definition("A", [](){ });
     // void* A;
-    // void* A;
-    auto A = myTaskflowLib.task_definition("A", test);
     void* B;
     void* C;
     myTaskflowLib.add_dependency(C, B);
